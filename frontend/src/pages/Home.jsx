e10a14777d2fc10610473c0fba44bef0add47ee6
goodjian <goodjian7@gmail.com>
2024-07-02 11:00:16 +0900

    3_03_navigation_debug_complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,133 +8,45 @@ import QuestionCreate from "./QuestionCreate";
 import QuestionList from "./QuestionList";
 
 const Home = () => {    
-    let [questionList, setQuestionList] = useState([])    
-
-    const navigate = useNavigate()
+    const apiUrl = import.meta.env.VITE_API_URL
     const [searchParams, setSearchParams] = useSearchParams()    
-    const pageOnSearchParam = Number(searchParams.get('page')) || 0
-    let [pageSize, setPageSize] = useState(10)
-    let [pageCount, setPageCount] = useState(0)
+    const pageIndex = Number(searchParams.get('page')) || 0         
+    console.log("Home rednered, pageIndex : " + pageIndex)
 
-    console.log("pageOnSearchParam : " + pageOnSearchParam)
+    let [displayCount, setDisplayCount] = useState(10)    
+    let [questionListInfo, setQuestionListInfo] = useState({
+        questionList:[],
+        totalCount:0,        
+    })        
+    
+    const getQuestionList = async ()=>{
+        try{            
+            let response = await axios.get(apiUrl+`/api/question/list?offset=${pageIndex*displayCount}&limit=${displayCount}`)                                    
+            let newQuestionListInfo = {questionList:response.data.question_list, totalCount:Number(response.data.total)}            
+            setQuestionListInfo(newQuestionListInfo)
+            console.log("getQuestionList called")
+
+        }
+        catch(error){
+            console.log("getQuestionList error")
+        }
+    }
+
+    useEffect(()=>{
+        console.log("effect from Home")
+        getQuestionList()
+    },[pageIndex, displayCount])
 
     return (
         <>
-        <QuestionList pageIndex={pageOnSearchParam} pageSize={pageSize} pageCount={pageCount} />
-        
-
-        <div onClick={()=>{setPageSize(pageSize+1)}}>setPageSize++</div>
-        <div onClick={()=>{setPageCount(pageCount+1)}}>setPageCount++</div>
+        <QuestionList             
+            questionList={questionListInfo.questionList}
+            pageIndex={pageIndex}
+            totalCount={questionListInfo.totalCount}
+            displayCount={displayCount}
+        />
         </>
     )
-           
-    // const getQuestionList = async(offset, limit)=>{
-    //     try{            
-    //         const response = await axios.get(apiUrl+`/api/question/list?offset=${offset}&limit=${limit}`)            
-    //         setQuestionList(response.data.question_list)    
-            
-    //         let newPageInfo = produce(pageInfo, (draft)=>{draft.pageCount=Math.ceil(Number(response.data.total) / pageInfo.pageSize)})            
-    //         setPageInfo(newPageInfo)
-    //     }
-    //     catch(error){
-    //         console.log("error while get question list")
-    //         setQuestionList([])
-    //     }            
-    // }
-
-    // useEffect(()=>{                            
-    //     let offset = pageInfo.pageIndex*pageInfo.pageSize
-    //     let limit = pageInfo.pageSize
-    //     getQuestionList(offset,limit)          
-    // },[])
-
-    // return (
-    //     <>  
-    //         <div className="container my-3">                
-    //             <table className="table">
-    //                 <thead>
-    //                     <tr className="table-dark">
-    //                         <th>번호</th>
-    //                         <th>제목</th>
-    //                         <th>작성일자</th>
-    //                     </tr>
-    //                 </thead>
-    //                 <tbody>
-    //                     {
-    //                         questionList.map((question, idx)=>{                        
-    //                             return(
-    //                                 <tr key={question.id}>
-    //                                     <td>{idx+1}</td>                            
-    //                                     <td>
-    //                                         <Link to={`/detail/${question.id}`}>{question.subject}</Link>                                            
-    //                                     </td>
-    //                                     <td>{question.create_date}</td>                                    
-    //                                 </tr>                             
-    //                             );
-    //                         })        
-    //                     }   
-    //                 </tbody>
-    //             </table>
-                
-    //             {/* 페이징 처리 시작 */}
-    //             <ul className="pagination justify-content-center">
-    //                 {/* 이전버튼 */}
-    //                 <li className={classNames("page-item", {disabled:pageInfo.pageIndex<=0})}>
-    //                     <button 
-    //                         className="page-link"
-    //                         onClick={()=>{
-    //                           let newPageIndex = pageOnSearchParam-1    
-    //                           //setPageInfo({...pageInfo, pageIndex:newPageIndex})                             
-    //                           navigate(`/?page=${newPageIndex}`) 
-    //                         }}
-    //                     >
-    //                         이전
-    //                     </button>
-    //                 </li>
-
-    //                 {/* 페이지버튼 */}
-    //                 {
-    //                     rangeFromTo(-2,3,1).map((idx)=>{                            
-    //                         let numBtn = pageInfo.pageIndex + idx
-    //                         if(numBtn >= 0 && numBtn <= pageInfo.pageCount-1){
-    //                             return(
-    //                                 <li key= {idx }className={classNames("page-item", {active:numBtn===pageInfo.pageIndex})}>
-    //                                     <button
-    //                                         className="page-link"
-    //                                         onClick={()=>{
-    //                                             setPageInfo({...pageInfo, pageIndex:numBtn})
-    //                                         }}>
-    //                                         {numBtn}
-    //                                     </button>
-    //                                 </li>
-    //                             )
-    //                         }
-    //                     })
-
-    //                 }                    
-                    
-    //                 {/* 다음버튼 */}
-    //                 <li className={classNames("page-item", {disabled:pageInfo.pageIndex>=pageInfo.pageCount-1})}>
-    //                     <button 
-    //                         className="page-link"
-    //                         onClick={()=>{
-    //                             let newPageIndex = pageOnSearchParam+1    
-    //                             //setPageInfo({...pageInfo, pageIndex:newPageIndex})                            
-    //                             navigate(`/?page=${newPageIndex}`) 
-    //                         }}
-    //                     >
-    //                         다음
-    //                     </button>
-
-    //                 </li>
-    //             </ul>
-    //             {/* 페이징 처리 끝 */}
-
-    //             <Link className="btn btn-primary" to="/question-create">add</Link>
-    //             <button className="btn btn-secondary" onClick={()=>{setPageInfo({...pageInfo, pageIndex:1})}}>test</button>
-    //         </div>                    
-    //     </>
-    // );
 };
 
 export default Home
--- frontend/src/pages/QuestionList.jsx
+++ frontend/src/pages/QuestionList.jsx
@@ -1,18 +1,96 @@
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate, Link } from "react-router-dom"
+import classNames from "classnames";
+import {rangeFromTo} from "../utils"
 
-const QuestionList = ({pageIndex=0, pageSize=10, pageCount})=>{
-    const navigate = useNavigate()
-    console.log("QuestionList rendered")
+const QuestionList = ({questionList, pageIndex, displayCount, totalCount,})=>{
+    console.log("QuestionList is rendered")
+    let minPageIndex = 0
+    let maxPageIndex = Math.ceil(totalCount/displayCount)    
+    const navigate = useNavigate()   
     return (
-        <>
-        <h1>QuestionList</h1>
-        <div>{pageIndex}</div>
-        <div>{pageSize}</div>
-        <div>{pageCount}</div>
-        <div onClick={()=>navigate(`/?page=${pageIndex-1}`)}>이전</div>
-        <div onClick={()=>navigate(`/?page=${pageIndex+1}`)}>다음</div>
+        <>  
+            <div className="container my-3">                
+                <table className="table">
+                    <thead>
+                        <tr className="table-dark">
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성일자</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            questionList.map((question, idx)=>{                        
+                                return(
+                                    <tr key={question.id}>
+                                        <td>{idx+1}</td>                            
+                                        <td>
+                                            <Link to={`/detail/${question.id}`}>{question.subject}</Link>                                            
+                                        </td>
+                                        <td>{question.create_date}</td>                                    
+                                    </tr>                             
+                                );
+                            })        
+                        }   
+                    </tbody>
+                </table>
+                
+                {/* 페이징 처리 시작 */}
+                <ul className="pagination justify-content-center">
+                    {/* 이전버튼 */}
+                    <li className={classNames("page-item", {disabled:pageIndex<=minPageIndex})}>
+                        <button 
+                            className="page-link"
+                            onClick={()=>{                                                               
+                              navigate(`/?page=${pageIndex-1}`) 
+                            }}
+                        >
+                            이전
+                        </button>
+                    </li>
+
+                    {/* 페이지버튼 */}
+                    {
+                        rangeFromTo(-2,3,1).map((idx)=>{                            
+                            let newPageIndex = pageIndex + idx
+                            if(newPageIndex >= 0 && newPageIndex <= maxPageIndex){
+                                return(
+                                    <li key= {idx }className={classNames("page-item", {active:newPageIndex===pageIndex})}>
+                                        <button
+                                            className="page-link"
+                                            onClick={()=>{
+                                                navigate(`/?page=${newPageIndex}`)
+                                            }}>
+                                            {newPageIndex}
+                                        </button>
+                                    </li>
+                                )
+                            }
+                        })
+
+                    }                    
+                    
+                    {/* 다음버튼 */}
+                    <li className={classNames("page-item", {disabled:pageIndex>=maxPageIndex})}>
+                        <button 
+                            className="page-link"
+                            onClick={()=>{                                
+                                navigate(`/?page=${pageIndex+1}`) 
+                            }}
+                        >
+                            다음
+                        </button>
+
+                    </li>
+                </ul>
+                {/* 페이징 처리 끝 */}
+
+                {/* 새 질문 등록 */}
+                <Link className="btn btn-primary" to="/question-create">add</Link>                
+            </div>                    
         </>
-    )
+    );
 }
 
 export default QuestionList
